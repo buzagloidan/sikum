5a0c38d2a0c950434ea36b679b73b66737a5eeb8
buza <buzagloidan@gmail.com>
2024-11-11 14:35:44 +0200

    Initial Commit

---
+++ sikum/sikumApp.swift
@@ -0,0 +1,17 @@
+//
+//  sikumApp.swift
+//  sikum
+//
+//  Created by Idan Buzaglo on 11/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct sikumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
